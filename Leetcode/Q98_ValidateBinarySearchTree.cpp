94e8599f4cc78577482aae81729b284946417011
JUSTINWU <justinwu258@hotmail.com>
2016-12-06 09:31:13 +0800

    [2016.12.06] [Leetcode Q98] code revise (by skip non-necessary if-else check)

--- Leetcode/Q98_ValidateBinarySearchTree.cpp
+++ Leetcode/Q98_ValidateBinarySearchTree.cpp
@@ -25,16 +25,11 @@ public:
             }
         }
         int smallest = INT_MIN;
-        int tmpMIN = INT_MIN;
-        // cout << "ans.size = " << ans.size()<< endl;
-        //cout << (-2147483647 > 2147483646) << endl;
         for(int i=0;i<ans.size();++i){
-            // cout << "ans.at(i) = " << ans.at(i) << ", smallest = " << smallest << "ans.at(i)>smallest = " << (ans.at(i)>smallest) << endl;
             if(ans.size() == i+1) ;
             else if(ans.at(i) == ans.at(i+1)) return false; //there are same nodes in tree
             
-            if(ans.at(i)>smallest) smallest = ans.at(i);
-            else if(smallest==tmpMIN) {++smallest; ++tmpMIN;}
+            if(ans.at(i)>=smallest) smallest = ans.at(i);
             else return false; //in-order visit, if there is node small than previous then it is not BST
         }
         return true;
